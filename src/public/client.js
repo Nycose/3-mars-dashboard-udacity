6ade6f4a58da5a7bdb6a4f740bf1b5239ab3f3e4
Mason Crawford <90871711+Nycose@users.noreply.github.com>
2021-11-08 11:38:42 -0600

    Update client.js

--- src/public/client.js
+++ src/public/client.js
@@ -24,7 +24,7 @@ const updateStore = (state, newState) => {
 }
 
 // renders app after state changes
-const render = async (root, state) => {
+const render = (root, state) => {
     root.innerHTML = App(state)
 }
 
